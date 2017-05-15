368a23988e022c2fed599b94835e732e1577e9ff
cargamoni <ahmetcanirdem@hotmail.com>
2017-05-16 01:15:32 +0300

    updates

--- secretImageShare/Form1.Designer.cs
+++ secretImageShare/Form1.Designer.cs
@@ -37,112 +37,206 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(13, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(9, 29);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(171, 134);
+            this.pictureBox1.Size = new System.Drawing.Size(256, 206);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 178);
+            this.button1.Location = new System.Drawing.Point(18, 313);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(171, 23);
+            this.button1.Size = new System.Drawing.Size(280, 36);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Resim Yükle";
+            this.button1.Text = "Örten Resim Yükle";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // buttonEncode
             // 
-            this.buttonEncode.Location = new System.Drawing.Point(190, 178);
+            this.buttonEncode.Location = new System.Drawing.Point(307, 313);
+            this.buttonEncode.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonEncode.Name = "buttonEncode";
-            this.buttonEncode.Size = new System.Drawing.Size(171, 23);
+            this.buttonEncode.Size = new System.Drawing.Size(280, 36);
             this.buttonEncode.TabIndex = 2;
-            this.buttonEncode.Text = "Gri  Yapma";
+            this.buttonEncode.Text = "Gizli Resim Yükle";
             this.buttonEncode.UseVisualStyleBackColor = true;
             this.buttonEncode.Click += new System.EventHandler(this.buttonEncode_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(367, 178);
+            this.button3.Location = new System.Drawing.Point(597, 313);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(171, 23);
+            this.button3.Size = new System.Drawing.Size(280, 36);
             this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
+            this.button3.Text = "Resmi Gizle";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // textBoxDizin
             // 
-            this.textBoxDizin.Location = new System.Drawing.Point(12, 152);
+            this.textBoxDizin.Location = new System.Drawing.Point(18, 273);
+            this.textBoxDizin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxDizin.Name = "textBoxDizin";
             this.textBoxDizin.ReadOnly = true;
-            this.textBoxDizin.Size = new System.Drawing.Size(171, 20);
+            this.textBoxDizin.Size = new System.Drawing.Size(278, 27);
             this.textBoxDizin.TabIndex = 4;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(190, 152);
+            this.textBox2.Location = new System.Drawing.Point(307, 273);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(171, 20);
+            this.textBox2.Size = new System.Drawing.Size(278, 27);
             this.textBox2.TabIndex = 5;
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(190, 12);
+            this.pictureBox2.Location = new System.Drawing.Point(9, 29);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(171, 134);
+            this.pictureBox2.Size = new System.Drawing.Size(256, 206);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 7;
             this.pictureBox2.TabStop = false;
             // 
             // pictureBox3
             // 
-            this.pictureBox3.Location = new System.Drawing.Point(367, 12);
+            this.pictureBox3.Location = new System.Drawing.Point(9, 29);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(171, 134);
+            this.pictureBox3.Size = new System.Drawing.Size(256, 206);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 8;
             this.pictureBox3.TabStop = false;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(367, 152);
+            this.textBox1.Location = new System.Drawing.Point(597, 273);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(171, 20);
+            this.textBox1.Size = new System.Drawing.Size(278, 27);
             this.textBox1.TabIndex = 9;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(18, 379);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(415, 25);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Resim Aynı Boyutta Kalsın* (*Siyah Beyaz Olacaktır)";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(198, 415);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(220, 28);
+            this.comboBox1.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 419);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 21);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Gizleme Oranı Seçiniz";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(18, 19);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(280, 244);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Örten Resim";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.pictureBox2);
+            this.groupBox2.Location = new System.Drawing.Point(307, 19);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Size = new System.Drawing.Size(280, 244);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Gizlenecek Resim";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.pictureBox3);
+            this.groupBox3.Location = new System.Drawing.Point(597, 19);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Size = new System.Drawing.Size(280, 244);
+            this.groupBox3.TabIndex = 15;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Şifreli Resim";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(583, 276);
+            this.ClientSize = new System.Drawing.Size(899, 467);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBoxDizin);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.buttonEncode);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Myriad Pro", 12.25F, System.Drawing.FontStyle.Bold);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -159,6 +253,12 @@
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
     }
 }
 
--- secretImageShare/Form1.cs
+++ secretImageShare/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AForge.Imaging.Filters;
 
 namespace secretImageShare
 {
@@ -20,11 +22,16 @@ namespace secretImageShare
         private void Form1_Load(object sender, EventArgs e)
         {
             this.Text = "Gizli Resim Paylaşma";
+            for (int i = 1; i <= 7; i++)
+                comboBox1.Items.Add(i);
+            comboBox1.SelectedIndex = 0;
+            comboBox1.Enabled = false;
+            label1.Enabled = false;
+            checkBox1.Enabled = false;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //File Dialog classının değişkene aktarılması.
             OpenFileDialog openDialog = new OpenFileDialog();
             openDialog.Filter = "Image Files (*.png, *.jpg, *.bmp) | *.png; *.jpg; *.bmp";    //Filter ile seçilebilecek tipteki resimler seçiliyor.
             openDialog.InitialDirectory = @"D:\Git Repositories\Secret-Image-Sharing\";        //İlk Açılacak Dosya Dizini
@@ -38,40 +45,86 @@ namespace secretImageShare
 
         private void buttonEncode_Click(object sender, EventArgs e)
         {
-            //Seçilen Resmin Bitmap olarak ayarlanması
-            Bitmap img = new Bitmap(textBoxDizin.Text);
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter = "Image Files (*.png, *.jpg, *.bmp) | *.png; *.jpg; *.bmp";    //Filter ile seçilebilecek tipteki resimler seçiliyor.
+            openDialog.InitialDirectory = @"D:\Git Repositories\Secret-Image-Sharing\";        //İlk Açılacak Dosya Dizini
 
-            for(int i = 0; i <= img.Width -1; i++)
+            if (openDialog.ShowDialog() == DialogResult.OK)
             {
-                for(int j = 0; j<= img.Height -1; j++)
-                {
-                    Color pixel = img.GetPixel(i, j);
-                    if(i < 1 && j < 10)
-                    {
-                        Console.Write("R = [" + i + "][" + j + "] = " + pixel.R);
-                        Console.Write(" G = [" + i + "][" + j + "] = " + pixel.G);
-                        Console.Write(" B = [" + i + "][" + j + "] = " + pixel.B + "\n");
-                    }
-                    //Bir resmin RGB değerlerinin toplamı 3e bölümü gri olur
-                    img.SetPixel(i, j, Color.FromArgb((pixel.R + pixel.G + pixel.B) / 3,(pixel.R + pixel.G + pixel.B) / 3,(pixel.R + pixel.G + pixel.B) / 3));
-                }
+                textBox2.Text = openDialog.FileName.ToString();         //Seçilen dosyanın dizininin textbox içerisine aktarılması
+                pictureBox2.ImageLocation = textBox2.Text;              //PictureBox üzerinde seçilen resmin gösterilmesi.
             }
+        }
 
-            //Gri tonlamalı resmin kaydedilmesi
-            SaveFileDialog saveFile = new SaveFileDialog();
-            saveFile.Filter = "Image Files (*.png, *.jpg) | *.png; *.jpg";
-            saveFile.InitialDirectory = @"D:\Git Repositories\Secret-Image-Sharing\";
+        public byte[] resimBiti(byte tekPixel)
+        {
+            int pixel = 0;
+            pixel = (int)tekPixel;
+            BitArray bitler = new BitArray(new byte[] { (byte)pixel });
+            bool[] boolDizi = new bool[bitler.Count];
+            bitler.CopyTo(boolDizi, 0);
+            byte[] bitDizisi = boolDizi.Select(bit => (byte)(bit ? 1 : 0)).ToArray();
+            Array.Reverse(bitDizisi);
+            return bitDizisi;
+        }
 
-            if (saveFile.ShowDialog() == DialogResult.OK)
-            {
-                textBox2.Text = saveFile.FileName.ToString();
-                pictureBox2.ImageLocation = textBox2.Text;
-                img.Save(textBox2.Text);
-            }
+        private byte resimByte(byte[] bitler)
+        {
+            String olusanBit = "";
+            for (int i = 0; i < 8; i++)
+                olusanBit += bitler[i];
+            byte yeniPixel = Convert.ToByte(olusanBit, 2);
+            int donusturulenPixel = (int)yeniPixel;
+            return (byte)donusturulenPixel;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
+            Bitmap orten = new Bitmap(textBoxDizin.Text);
+            Bitmap gizli = new Bitmap(textBox2.Text);
+            for (int i = 0; i <= gizli.Width - 1; i++)
+                for (int j = 0; j <= gizli.Height - 1; j++)
+                {
+                    Color pixel = gizli.GetPixel(i, j);
+                    //Bir resmin RGB değerlerinin toplamı 3e bölümü gri olur
+                    gizli.SetPixel(i, j, Color.FromArgb((pixel.R + pixel.G + pixel.B) / 3, (pixel.R + pixel.G + pixel.B) / 3, (pixel.R + pixel.G + pixel.B) / 3));
+                }
+
+            pictureBox2.Image = gizli;
+            Color ortenResimRenk = new Color();
+            Color gizliResimRenk = new Color();
+
+            byte[] gizliBit;
+            byte[] kirmiziBit;
+            byte[] yesilBit;
+            byte[] maviBit;
+
+            byte yeniGizliBit = 0;
+            byte yeniKirmiziBit = 0;
+            byte yeniYesilBit = 0;
+            byte yeniMaviBit = 0;
+
+            for(int i = 0; i <= orten.Height -1; i++)
+            {
+                for(int j = 0; j <= orten.Width -1; j++)
+                {
+                    ortenResimRenk = orten.GetPixel(i, j);        
+                }
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked)
+            { 
+                label1.Enabled = false;
+                comboBox1.Enabled = false;                
+            }
+            else
+            {
+                label1.Enabled = true;
+                comboBox1.Enabled = true;
+            }
         }
     }
 }
